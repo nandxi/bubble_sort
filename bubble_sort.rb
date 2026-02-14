dd47665e9763fcda07a758d899bfb74b8bd43852
nandan <230424156+nandxi@users.noreply.github.com>
2026-02-14 21:42:19 +0530

    Create bubble_sort method that returns sorted array

---
+++ bubble_sort.rb
@@ -0,0 +1,19 @@
+# Goal
+# > bubble_sort([4,3,78,2,0,2])
+# => [0,2,2,3,4,78]
+
+def bubble_sort(arr)
+  pass = 0
+  arr.each do |num|
+    pass += 1
+    arr[0, arr.length-pass].each_index do |i|
+      if arr[i] > arr[i+1]
+        temp = arr[i]
+        arr[i] = arr[i+1]
+        arr[i+1] = temp
+      end
+    end
+  end
+end
+
+p bubble_sort([3,2,1,0,-1])
