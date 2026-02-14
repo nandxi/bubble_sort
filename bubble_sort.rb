4240811f715adc14dc6a600c4e85dfc621f22667
nandan <230424156+nandxi@users.noreply.github.com>
2026-02-14 21:54:46 +0530

    Optimize by adding a flag to check if array is sorted
    I initialized a variable named sorted and set it to true. If at some pass through the array we are swapping elements it will flip to false which means that the array is not sorted.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,15 +3,18 @@
 # => [0,2,2,3,4,78]
 
 def bubble_sort(arr)
+  sorted = true
   pass = 0
   arr.each do |num|
     pass += 1
     arr[0, arr.length-pass].each_index do |i|
       if arr[i] > arr[i+1]
+        sorted = false
         temp = arr[i]
         arr[i] = arr[i+1]
         arr[i+1] = temp
       end
+      return arr if sorted == true
     end
   end
 end
